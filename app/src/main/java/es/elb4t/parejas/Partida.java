58ba8a76eef821dbdc762f86c593d964537af53a
Eloy Llacer <eloy4t@gmail.com>
2017-07-06 23:45:25 +0200

    Conectar con Google Play Games una aplicación Android

--- app/src/main/java/es/elb4t/parejas/Partida.java
+++ app/src/main/java/es/elb4t/parejas/Partida.java
@@ -1,5 +1,7 @@
 package es.elb4t.parejas;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
 /**
  * Created by eloy on 6/7/17.
  */
@@ -12,5 +14,6 @@ public class Partida {
     public static int puntosJ1;
     public static int puntosJ2;
     public static String tipoPartida="LOCAL";
+    public static GoogleApiClient mGoogleApiClient;
 
 }
